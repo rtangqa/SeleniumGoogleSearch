763fea22946ca8ce865bc941f9db4110c2a68bef
rtangqa <rtangrtang@gmail.com>
2016-05-18 13:05:09 -0700

    Update TestGoogleSearch.java

--- src/GoogleSearch/TestGoogleSearch.java
+++ src/GoogleSearch/TestGoogleSearch.java
@@ -23,9 +23,9 @@ public class TestGoogleSearch {
 
     	        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("coelacanths");
     	        driver.findElement(By.xpath("//button[@name='btnG']")).click();
-    	        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    	        driver.manage().timeouts().implicitlyWait(130, TimeUnit.SECONDS);
     	        driver.findElement(By.partialLinkText("Wikipedia")).click();
-    	        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    	        driver.manage().timeouts().implicitlyWait(130, TimeUnit.SECONDS);
     	        System.out.println("The current URL has changed to: " + str);
     	   }
 }
